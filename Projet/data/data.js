bc74cb63c9450cdb95199d9b7d1803f54e3d6f22
Shadow9275 <86415268+Shadow9275@users.noreply.github.com>
2024-12-04 11:06:55 -0500

    update3

--- Projet/data/data.js
+++ Projet/data/data.js
@@ -1,18 +1,17 @@
-const dataList = [
-    {
-      title: "Vue.js",
-      def: "Vue.js is a progressive JavaScript framework for building user interfaces.",
-      img: "https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg"
-    },
-    {
-      title: "JavaScript",
-      def: "JavaScript is a programming language commonly used in web development.",
-      img: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
-    },
-    {
-      title: "CSS",
-      def: "CSS is a style sheet language used for describing the presentation of a document.",
-      img: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
-    }
-  ];
-  
+const dataItems = [
+  {
+      title: "Toronto",
+      desc: "Toronto est la plus grande ville du Canada et un centre culturel majeur. Connue pour la tour CN et ses quartiers diversifiés.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Toronto_August_2017.jpg/1280px-Toronto_August_2017.jpg"
+  },
+  {
+      title: "Montagnes Rocheuses",
+      desc: "Les montagnes Rocheuses sont une chaîne de montagnes emblématique offrant des paysages spectaculaires, des parcs nationaux et des activités en plein air.",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM4z2jKHPdfPqLhTN3S_waeuMf1GRNnFEpgg&s"
+  },
+  {
+      title: "Québec",
+      desc: "Québec est une province riche en culture francophone, célèbre pour sa ville historique et ses festivals animés.",
+      img: "https://voyages.destinationcanada.com/_next/image?url=https%3A%2F%2Fadmin.destinationcanada.com%2Fsites%2Fdefault%2Ffiles%2F2023-06%2FQC-Chateau-Frontenac_hero.jpg&w=1920&q=75"
+  }
+];
---
+++ projet_V2/data/data.js
@@ -0,0 +1,19 @@
+const dataList = [
+    {
+      title: "Space Invader 1",
+      desc: "Trouvé près de la Tour Eiffel avec une mosaïque colorée.",
+      img: "https://via.placeholder.com/150",
+    },
+    {
+      title: "Space Invader 2",
+      desc: "Repéré à Montmartre, très complexe et impressionnant.",
+      img: "https://via.placeholder.com/150",
+    },
+    {
+      title: "Space Invader 3",
+      desc: "Près de Notre-Dame, avec un motif rétro unique.",
+      img: "https://via.placeholder.com/150",
+    },
+  ];
+
+  
