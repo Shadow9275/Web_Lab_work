652e2d63b9c1cf5ebcfcf9a6b3a7fd4653483ee0
Shadow9275 <86415268+Shadow9275@users.noreply.github.com>
2024-12-04 09:32:40 -0500

    up

--- Projet/data/data.js
+++ Projet/data/data.js
@@ -1,17 +1,18 @@
-const dataItems = [
+const dataList = [
     {
-        title: "Vue.js Introduction",
-        desc: "Vue.js is a progressive JavaScript framework for building user interfaces.",
-        img: "https://via.placeholder.com/300x200"
+      title: "Vue.js",
+      def: "Vue.js is a progressive JavaScript framework for building user interfaces.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg"
     },
     {
-        title: "Responsive Web Design",
-        desc: "Responsive Web Design makes your web page look good on all devices.",
-        img: "https://via.placeholder.com/300x200"
+      title: "JavaScript",
+      def: "JavaScript is a programming language commonly used in web development.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
     },
     {
-        title: "JavaScript Basics",
-        desc: "JavaScript is a programming language that helps to implement complex features on web pages.",
-        img: "https://via.placeholder.com/300x200"
+      title: "CSS",
+      def: "CSS is a style sheet language used for describing the presentation of a document.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
     }
-];
+  ];
+  
